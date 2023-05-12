1cf33b3d8ab4f17c06695b31a239b76730f2b716
Nolawi Gerawork <nolawigerawork@gmail.com>
2023-05-12 16:44:44 -0400

    Adds basic file creation executable.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-int main() {
-  std::cout << "hello world\n";
-  return 0;
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2){ 
+      std::cout << "Usage: ./main.cpp filename \n";
+      return 1;
+    }
+
+    // Create file
+    std::ofstream outfile(argv[1]);
+    if(!outfile) {
+        std::cout << "Error opening file.\n";
+        return 1;
+    }
+
+    outfile.close();
+    std::cout << "Successfully created " << argv[1] << "\n";
+    return 0;
 }
