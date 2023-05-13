34b4c434831cee4ac53b37148a5abc1b3d1ec2df
Nader Daruvala <nader@pop-os.localdomain>
2023-05-13 01:40:36 -0400

    Got writing to file (overrights existing stuff)

--- build/CMakeFiles/CmdLineTextEditor.dir/DependInfo.cmake
+++ build/CMakeFiles/CmdLineTextEditor.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/nolawigerawork/documents/projects/CommandLineTxtEditor/src/main.cpp" "CMakeFiles/CmdLineTextEditor.dir/src/main.cpp.o" "gcc" "CMakeFiles/CmdLineTextEditor.dir/src/main.cpp.o.d"
+  "/home/nader/Projects/CommandLineTxtEditor/src/main.cpp" "CMakeFiles/CmdLineTextEditor.dir/src/main.cpp.o" "gcc" "CMakeFiles/CmdLineTextEditor.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
